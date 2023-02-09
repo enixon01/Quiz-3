11a906e77a1f0a36b6ec72e5806b6242f8fb891a
Emily Nixon <123026549+enixon01@users.noreply.github.com>
2023-02-09 14:08:26 -0500

    Add files via upload

---
+++ Quiz 3 .py
@@ -0,0 +1,17 @@
+#Emily Nixon
+#"43" = expression
+#speed=47 is a assignment statement
+#message = "They have arrived" is an assignment statement
+#import turtle is a function statement
+#print("The value is", value) is a print statement
+#To access the functions in the math module, we would use the command import math
+#To call a function in a math module we would do math.function. For example: math.sin
+def draw_circle(radius):
+import math
+def net_value(salary):
+    value = 0.14*(salary-100)+0.24*(salary+1000)
+    print("The net value of a salary of", salary,"dollars per week is $",value)
+net_value(200)
+def next_stop(location):
+    print("Next stop is:",location,"miles away.")
+next_stop(5)
